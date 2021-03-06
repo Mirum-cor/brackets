de8681ccf5966fd34449b710ba524284caeb54ed
Mirum-cor <laetitia-caritas@mail.ru>
2021-03-06 17:27:22 +0300

    feat: task is solved (tests are passed)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openingBrackets = bracketsConfig.map((arr) => arr[0]);
+    const closingBrackets = bracketsConfig.map((arr) => arr[1]);
+    const strBracketsArray = str.split("");
+    const openedBrackets = [];
+    for (let i = 0; i < strBracketsArray.length; i++) {
+        const currentBracket = strBracketsArray[i];
+        const bracketPosition = closingBrackets.indexOf(currentBracket);
+        if (openingBrackets.includes(currentBracket)) {
+            if (
+                closingBrackets.includes(currentBracket) &&
+                openedBrackets[openedBrackets.length - 1] ===
+                    openingBrackets[bracketPosition]
+            ) {
+                openedBrackets.pop();
+            } else {
+                openedBrackets.push(currentBracket);
+            }
+        } else if (closingBrackets.includes(currentBracket)) {
+            if (
+                strBracketsArray[i - 1] === openingBrackets[bracketPosition] ||
+                (closingBrackets.includes(strBracketsArray[i - 1]) &&
+                    openedBrackets[openedBrackets.length - 1] ===
+                        openingBrackets[bracketPosition])
+            ) {
+                openedBrackets.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    if (openedBrackets.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
